2870ee706f396c48b9bd95d9de183ba155d0bf4f
don <donsongit@163.com>
2021-06-09 19:43:10 +0800

    auto set monitor and fix background

--- autostart.sh
+++ autostart.sh
@@ -3,14 +3,6 @@
 LOC=$(readlink -f "$0")
 DIR=$(dirname "$LOC")
 
-# wall paper
-while true
-do
-	bash $DIR/dwm-bg.sh
-	sleep 50m
-done &
-
-
 # touchpad scorll horization
 id=`xinput --list | grep "Touchpad" | cut -d '=' -f2 | cut -d '[' -f1`
 scroll_id=`xinput --list-props ${id} | grep "Two-Finger Scrolling" | cut -d '(' -f2 | cut -d ')' -f1`
@@ -20,15 +12,21 @@ xinput --set-prop $id $scroll_id 1 1
 tap_id=`xinput --list-props ${id} | grep "Tap Action" | cut -d '(' -f2 | cut -d ')' -f1`
 xinput --set-prop $id $tap_id 1 1 1 1 1 1
 
+# more monitor
+bash $DIR/dwm-screen.sh
+
 # brightness
 bash $DIR/dwm-brightness.sh
 
+# wall paper
+while true
+do
+	bash $DIR/dwm-bg.sh
+	sleep 50m
+done &
+
 # open bluetooth
 bluetooth on
-
-# more monitor
-# ./dwmbar-functions/dwm_monitor.sh
-# xrandr --output eDP-1-1 --off
 
 # power manager
 xfce4-power-manager --daemon
@@ -39,7 +37,7 @@ picom -b
 # dwm window manager name tool
 wmname LG3D
 
-xsetroot -name " initial wifi... "
+xsetroot -name " Initial WIFI... "
 # networkmanager and dwm-status
 wifi_up=0
 while true
@@ -48,10 +46,10 @@ do
 		wifi_up=1
 		res=$(bash $DIR/dwmbar-functions/dwm_wifi.sh)
 		if (($res > 0));then 
-			xsetroot -name " wifi connected failed!!! "
+			xsetroot -name " WIFI connected failed!!! "
 			sleep 10
 		fi
-		xsetroot -name " wifi is connected "
+		xsetroot -name " WIFI is connected "
 	fi
 	bash $DIR/dwm-status-refresh.sh
 	sleep 3
---
+++ dwm-screen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+list=($(xrandr | grep ' connected' | awk '{print $1}'))
+
+count=${#list[@]}
+
+if [ $count == 3 ]; then
+	xrandr --output DP-0 --mode 1920x1080 --rate 60 --pos 4480x0 --rotate left --output DP-1 --off --output HDMI-0 --primary --mode 2560x1440 --rate 60 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --rate 60 --pos 0x180 --rotate normal
+fi
