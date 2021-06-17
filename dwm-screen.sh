9a84ee314edab4b62e87e12f447655e5f10bbbd5
don <donsongit@163.com>
2021-06-17 20:56:58 +0800

    update dwm screen

--- dwm-screen.sh
+++ dwm-screen.sh
@@ -6,6 +6,7 @@ count=${#list[@]}
 
 if [ $count == 3 ]; then
 	xrandr --output DP-0 --mode 1920x1080 --rate 60 --pos 4480x0 --rotate left --output DP-1 --off --output HDMI-0 --primary --mode 2560x1440 --rate 60 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --rate 60 --pos 0x180 --rotate normal
+	nvidia-settings --assign CurrentMetaMode="DP-0: nvidia-auto-select +4480+0 {rotation=left, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
 elif [ $count == 2 ]; then
 	xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --rate 144 --pos 0x0 --rotate normal
 fi
