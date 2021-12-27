1d7a208a41f1af902c19a143a0a145353b135a50
don <donsongit@163.com>
2021-12-27 16:50:09 +0800

    vpn-up

--- vpn-up.sh
+++ vpn-up.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-nmcli connection up hortor-vpn
+if ! nmcli connection up hortor-vpn; then
+    echo "connect to hortor-vpn failed"
+fi
 
-if [ "$1" == "" ]; then
+if [ -z "$WIFI_NAME" ]; then
+    exit 0
+fi
+
+active_wifi=$(nmcli con show --active | grep "$WIFI_NAME")
+if [ ! -z "$active_wifi" ]; then
 	./vpn-router.sh
 fi
